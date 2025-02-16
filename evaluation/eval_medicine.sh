696549fe960f2462d17f3bdfe789436bb69a5094
acl-2025-5830 <acl.anonymous4459@gmail.com>
2025-02-16 18:59:38 +0900

    Add files via upload

---
+++ evaluation/eval_medicine.sh
@@ -0,0 +1,30 @@
+HF_TOKEN="YOUR HUGGINGFACE TOKEN"
+OUTPUT_PATH="./"
+model_list=(
+    '/raid/MLP/mjkim/Test-Enhanced-Learning/saved_models_finance/Llama-3.2-1B-TEL'
+
+)
+
+N_SHOT=4
+
+for model_path in "${model_list[@]}"
+do
+    model_dir="$(dirname "${model_path}")"
+    model_name="$(basename "${model_dir}")"
+
+    echo "=========================================="
+    echo "model_path:  ${model_path}"
+    echo "model_dir:   ${model_dir}"
+    echo "model_name:  ${model_name}"
+    echo "=========================================="
+
+    accelerate launch -m lm_eval \
+        --model hf \
+        --tasks medmcqa,mmlu_continuation_clinical_knowledge,mmlu_continuation_medical_genetics,mmlu_continuation_anatomy,mmlu_continuation_professional_medicine,mmlu_continuation_college_biology,mmlu_continuation_college_medicine,headqa_en\
+        --batch_size 1 \
+        --model_args "pretrained=${model_path},dtype=bfloat16,token=${HF_TOKEN}" \
+        --output_path "${OUTPUT_PATH}" \
+        --trust_remote_code\
+        --num_fewshot ${N_SHOT}\
+        --seed 42
+done
